0429d621e44b378c849e4a0c070cf1a2dd690018
yoonheo0419@gmail.com <yoonheo0419@gmail.com>
2021-02-23 21:19:03 +0900

    .

--- javascript/browser101/05.2 shopping list/app.js
+++ javascript/browser101/05.2 shopping list/app.js
@@ -19,32 +19,26 @@ function onAdd() {
   input.value = "";
   input.focus();
 }
+
+let id = 0; /// UUID
+
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
+  itemRow.setAttribute("data-id", id);
 
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
+  itemRow.innerHTML = `
+    <div class = "item">
+      <span class="item__name">${text}</span>
+      <button class="item__delete" >
+        <i class="xi-trash" data-id=${id}></i>
+      </button>
+    </div>
+    <div class="item__divider"></div>
+  `;
 
-  const name = document.createElement("span");
-  name.setAttribute("class", "item__name");
-  name.innerText = text;
+  id++;
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = '<i class="xi-trash"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
   return itemRow;
 }
 
@@ -55,5 +49,13 @@ addBtn.addEventListener("click", () => {
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
