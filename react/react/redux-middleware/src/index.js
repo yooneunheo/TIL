45d1e32e0b5a214e550e020bb2f3f69b975c41d9
yoon <yoonheo0419@gmail.com>
2021-04-14 23:56:03 +0900

    .

--- react/react/redux-middleware/src/index.js
+++ react/react/redux-middleware/src/index.js
@@ -5,10 +5,15 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
